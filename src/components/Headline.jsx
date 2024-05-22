8c7c38ef1fe2e5c91fe1432c8dcea09f326acdb9
nerwinalamas <nerwinalamas@gmail.com>
2024-05-22 10:59:19 +0800

    style: enhancement in headline section

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -12,18 +12,21 @@ const Headline = () => {
 			</Link>
 			<div className="lg:flex lg:flex-row-reverse lg:relative">
 				<img src={PdfImage} alt="PDF Image" title="PDF Image" className="cursor-pointer mx-auto lg:w-96 lg:h-auto" />
-				<img src={CircleText} alt="You are enough image" className="hidden lg:block lg:absolute lg:-top-16 lg:-right-16 xl:-top-16 xl:right-4 animate-spin-slow" />
+				<img src={CircleText} alt="You are enough image" className="hidden lg:w-28 lg:h-28 lg:object-contain lg:block lg:absolute lg:-top-5 lg:-right-5 xl:w-32 xl:h-32 xl:object-contain xl:right-16 animate-spin-slow" />
 				<div className="xl:w-1/2 xl:py-14">
-					<h2 className="w-[95%] text-4xl mx-auto font-playfairDisplay font-bold leading-[45px] xl:w-[80%] xl:mx-0 xl:text-5xl">
+					<h2 className="w-[95%] text-4xl mx-auto font-playfairDisplay font-bold leading-[45px] xl:w-[80%] xl:mx-0 xl:text-5xl text-customBlack">
 						How to attract your dream partner.
 					</h2>
-					<p className="w-[95%] mx-auto mt-7 mb-6 text-xl leading-8 tracking-wide text-customGray xl:mx-0 xl:text-2xl">
+					<p className="w-[95%] mx-auto mt-7 mb-6 text-xl leading-8 tracking-wide text-customBlack xl:mx-0 xl:text-2xl">
 						Overcome the beliefs that block your ability to connect
 						with high quality men and learn the real reason why
 						smart, successful women are still single after 30.
 					</p>
-					<button className="w-full p-5 text-xl rounded-full font-bold bg-customOrange text-customWhite lg:w-80 xl:p-4 hover:bg-opacity-80">
+					<button className="w-full p-5 text-xl rounded-full font-bold bg-customOrange text-customWhite lg:hidden">
 						Download Your Guide
+					</button>
+					<button className="hidden lg:block lg:p-5 lg:text-xl lg:rounded-full lg:font-bold lg:bg-customOrange lg:text-customWhite lg:w-80 xl:p-4 hover:bg-opacity-80">
+						Download Your Guide Now
 					</button>
 				</div>
 			</div>
