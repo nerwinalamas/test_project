f48cb653fc2e7ee8f588f2629badc9bec08210f1
nerwinalamas <nerwinalamas@gmail.com>
2024-05-22 09:43:18 +0800

    fix alignment and capitalization

--- src/components/Achieve.jsx
+++ src/components/Achieve.jsx
@@ -3,10 +3,10 @@ import { achieveData } from "../data";
 const Achieve = () => {
 	return (
 		<div className="my-28 flex flex-col gap-20 lg:w-full">
-			<h1 className="font-playfairDisplay text-3xl font-bold text-center capitalize xl:text-4xl">
+			<h1 className="font-playfairDisplay text-3xl font-bold text-center xl:text-4xl">
 				What you’ll achieve:
 			</h1>
-			<div className="flex flex-col gap-24 lg:flex-row lg:flex-wrap">
+			<div className="flex flex-col gap-24 lg:flex-row lg:flex-wrap lg:gap-32">
 				{achieveData.map((data, index) => (
 					<div key={index} className={`flex flex-col gap-5 items-center xl:gap-1 ${index === 1 ? "xl:mt-28" : ""}`}>
 						<img src={data.image} alt={data.alternativeText} />
