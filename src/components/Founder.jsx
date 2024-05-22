ca3e39953ac35406938a25156c7b51368a9aab4c
nerwinalamas <nerwinalamas@gmail.com>
2024-05-22 13:23:15 +0800

    style: fix alignment of founder section and add button for large screen

--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -7,13 +7,13 @@ const Founder = () => {
 				Meet our founder,{" "}
 				<span className="text-customPeach">Marni</span>
 			</h1>
-			<img src={Marni} title="Marni" alt="Marni Image" className="cursor-pointer mt-14 mb-16" />
+			<img src={Marni} title="Marni" alt="Marni Image" className="cursor-pointer mt-14 mb-16 xl:w-96 xl:h-full xl:object-contain" />
 			<div className="xl:w-max">
-				<h1 className="hidden w-96 text-5xl font-playfairDisplay font-bold leading-[50px] lg:block">
+				<h1 className="hidden w-96 text-5xl font-playfairDisplay font-bold leading-[50px] lg:block lg:leading-[50px]">
 					Meet our founder,{" "}
 					<span className="text-customPeach">Marni</span>
 				</h1>
-				<p className="w-[309px] mt-2 mb-16 text-xl leading-8 tracking-wide text-customGray md:w-full xl:w-[650px] lg:text-lg lg:mb-10 lg:mt-5">
+				<p className="w-[309px] mt-2 mb-16 text-xl leading-8 tracking-wide text-customGray md:w-full xl:w-[500px] lg:text-lg lg:mb-5 lg:mt-5">
 					Having been married for 17 years, divorced, and successfully
 					dated again in today’s complex social environment, and now
 					happily married, Marni understands what it feels like to be
@@ -27,8 +27,11 @@ const Founder = () => {
 					fulfilling life relationships was truly the mission of her
 					soul and purpose.
 				</p>
-				<button className="w-[90%] p-5 text-xl rounded-full font-bold bg-customOrange text-customWhite lg:w-80 lg:p-4 hover:bg-opacity-80">
+				<button className="w-[90%] p-5 text-xl rounded-full font-bold bg-customOrange text-customWhite lg:hidden hover:bg-opacity-80">
 					Learn More
+				</button>
+				<button className="hidden lg:block lg:text-lg lg:rounded-full lg:font-bold lg:bg-customOrange lg:text-customWhite lg:w-max lg:p-4 lg:px-10 lg:hover:bg-opacity-80">
+					Discover Marni's Proven Process
 				</button>
 			</div>
 		</div>
