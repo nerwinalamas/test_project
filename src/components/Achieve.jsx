c7e548b908252fe66fda2ec5ee29bc316f78e47b
nerwinalamas <nerwinalamas@gmail.com>
2024-05-22 13:20:59 +0800

    style: fix alignment on achieve section

--- src/components/Achieve.jsx
+++ src/components/Achieve.jsx
@@ -6,11 +6,11 @@ const Achieve = () => {
 			<h1 className="font-playfairDisplay text-3xl font-bold text-center xl:text-4xl">
 				What you’ll achieve:
 			</h1>
-			<div className="flex flex-col gap-24 lg:flex-row lg:flex-wrap xl:gap-32">
+			<div className="flex flex-col gap-24 lg:flex-row lg:flex-wrap justify-center xl:gap-20">
 				{achieveData.map((data, index) => (
-					<div key={index} className={`flex flex-col gap-5 items-center xl:gap-1 ${index === 1 ? "xl:mt-28" : ""}`}>
-						<img src={data.image} alt={data.alternativeText} />
-						<p className="w-[309px] mx-auto mt-7 mb-6 text-xl text-center leading-8 tracking-wide text-customGray xl:w-72">{data.description}</p>
+					<div key={index} className={`flex flex-col gap-5 items-center xl:gap-1 ${index === 1 ? "xl:mt-16" : ""}`}>
+						<img src={data.image} alt={data.alternativeText} className="lg:w-40 lg:h-40" />
+						<p className="w-[309px] mx-auto mt-7 mb-6 text-xl text-center leading-8 tracking-wide text-customGray lg:text-lg xl:w-72">{data.description}</p>
 					</div>
 				))}
 			</div>
